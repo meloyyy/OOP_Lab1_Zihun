fa8939956024c3732d6bf48f2443f66bf371fb75
Mykyta Zihun <m.r.zihun@student.khai.edu>
2025-12-25 13:00:33 +0200

    Добавьте файлы проекта.

---
+++ Movie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab1_Zihun
+{
+    class Movie
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public string Title { get; set; }
+        public Genre MovieGenre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public double Rating { get; set; }
+        public int Duration { get; set; }
+
+        public Movie(int id, string title, Genre genre, DateTime date, double rating, int duration)
+        {
+            this.id = id;
+            Title = title;
+            MovieGenre = genre;
+            ReleaseDate = date;
+            Rating = rating;
+            Duration = duration;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(Id + " | " + Title + " | " + MovieGenre + " | " + ReleaseDate.ToShortDateString() + " | " + Rating + " | " + Duration + " хв");
+        }
+    }
+}
